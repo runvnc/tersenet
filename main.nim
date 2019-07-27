2c39f64e71b240f9b80ffb4451fb25b89c25b89b
Jason Livesay <runvnc@gmail.com>
2019-07-27 16:13:39 -0700

    Update main.nim

--- main.nim
+++ main.nim
@@ -1,1 +1,1 @@
-# Note: this software does not currently exist.  I have too many projects already, such as the 3D Lua-programmable libretro emulator I am working on.
+# Note: this software does not currently exist.  I have too many projects already, such as the 3D Lua-programmable libretro emulator I am supposed to be working on right now.
